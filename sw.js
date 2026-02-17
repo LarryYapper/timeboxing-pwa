a9fda30a4f7a6215a7a8912d9a672a162e82caac
LarryYapper <filipkubik.mail@gmail.com>
2026-02-17 14:24:58 -0500

    Hotfix: Remove block entry animation to prevent flashing, v1.70

--- sw.js
+++ sw.js
@@ -3,17 +3,17 @@
  * Provides offline functionality and caching
  */
 
-const CACHE_NAME = 'timeboxing-v1.69';
+const CACHE_NAME = 'timeboxing-v1.70';
 const STATIC_ASSETS = [
     './',
     './index.html',
-    './css/style.v4.css?v=1.69',
-    './js/app.js?v=1.69',
-    './js/calendar.js?v=1.69',
-    './js/storage.js?v=1.69',
-    './js/routines.js?v=1.69',
-    './js/smartinput.js?v=1.69',
-    './js/timeblocks.js?v=1.69',
+    './css/style.v4.css?v=1.70',
+    './js/app.js?v=1.70',
+    './js/calendar.js?v=1.70',
+    './js/storage.js?v=1.70',
+    './js/routines.js?v=1.70',
+    './js/smartinput.js?v=1.70',
+    './js/timeblocks.js?v=1.70',
     './manifest.json',
     './icons/icon-192.svg',
     './icons/icon-512.svg'
